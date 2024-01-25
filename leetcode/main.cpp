cf555bde5667b1a0e12beca673e5250dac556546
Isaac Chen <isaacychen@outlook.com>
2024-01-25 12:32:59 -0500

    feat(CMakeLists.txt): add CMakeLists.txt file to project to enable building with CMake

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by isaac on 1/25/2024.
+//
+
+#include "main.h"
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+namespace cppp {
+} // cppp
---
+++ leetcode/main.h
@@ -0,0 +1,16 @@
+//
+// Created by isaac on 1/25/2024.
+//
+
+#ifndef CPP_PLAYGROUND_MAIN_H
+#define CPP_PLAYGROUND_MAIN_H
+
+namespace cppp {
+
+    class main {
+
+    };
+
+} // cppp
+
+#endif //CPP_PLAYGROUND_MAIN_H
